883540887db48f3e2ac1ed5398e0326e7dc5506d
hugosenari <hugosenari@gmail.com>
2022-03-12 00:08:32 -0300

    chore: re index echon

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = ''A small package to create lindenmayer-systems or l-systems.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."echon-master".url = "path:./master";
+  inputs."echon-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."echon-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."echon-1_0_0".url = "path:./1_0_0";
+  inputs."echon-1_0_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."echon-1_0_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A small package to create lindenmayer-systems or l-systems.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-Echon-master.flake = false;
+  inputs.src-Echon-master.owner = "eXodiquas";
+  inputs.src-Echon-master.ref   = "refs/heads/master";
+  inputs.src-Echon-master.repo  = "Echon";
+  inputs.src-Echon-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-Echon-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-Echon-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
